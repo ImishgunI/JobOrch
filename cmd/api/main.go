4771badc57eb317a30eb67c179c0cdc75434b545
Michael <mvasilyev04@gmail.com>
2026-02-19 22:59:52 +0300

    create graceful shutdown channel

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,17 @@
 package api
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sig
+		log.Println("Graceful shutdown signal recieved")
+	}()
+}
