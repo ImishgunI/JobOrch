97032f489ed7f21612538abc3b44cfbe20848068
Michael <mvasilyev04@gmail.com>
2026-02-20 16:09:14 +0300

    create simple worker pool to learn it

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,1 +1,23 @@
-package worker
+package main
+
+import (
+	"log"
+	"time"
+)
+
+const maxConcurrency = 3
+
+func main() {
+	sem := make(chan struct{}, maxConcurrency)
+	for i := range 5 {
+		sem <- struct{}{}
+		log.Printf("Start job %d\n", i)
+		go func(i int) {
+			defer func(i int) {
+				log.Printf("Finish Job %d\n", i)
+				<-sem
+			}(i)
+			time.Sleep(2 * time.Second)
+		}(i)
+	}
+}
