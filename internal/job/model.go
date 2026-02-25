ffc01feb2734e82f6f1841b30c717c42f1baa140
Michael <mvasilyev04@gmail.com>
2026-02-25 16:20:24 +0300

    create db-model struct

--- internal/job/model.go
+++ internal/job/model.go
@@ -1,7 +1,19 @@
 package job
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type Job interface {
 	Execute(ctx context.Context) error
+}
+
+type JobRecord struct {
+	ID        string
+	Status    Status
+	Payload   []byte
+	Error     *string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
