5b1c60460c09d37ef212d20e9a0f9d641c43b651
Michael <mvasilyev04@gmail.com>
2026-02-25 15:38:11 +0300

    call NewPool

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"log"
-	"time"
+	"JobOrch/internal/platform/workerpool"
+	"context"
 )
 
-const maxConcurrency = 3
-
 func main() {
-	sem := make(chan struct{}, maxConcurrency)
-	for i := range 5 {
-		sem <- struct{}{}
-		log.Printf("Start job %d\n", i)
-		go func(i int) {
-			defer func(i int) {
-				log.Printf("Finish Job %d\n", i)
-				<-sem
-			}(i)
-			time.Sleep(2 * time.Second)
-		}(i)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	pool := workerpool.NewPool(ctx, 10)
+	
+	cancel()
+	pool.ShutDown()
 }
