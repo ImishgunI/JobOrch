458d1692c69bdb845607f28c6ce4d5ad334bcd0b
Michael <mvasilyev04@gmail.com>
2026-02-25 16:20:44 +0300

    create contract for repo

--- internal/storage/interface.go
+++ internal/storage/interface.go
@@ -1,3 +1,13 @@
 package storage
 
-type Repository interface{}
+import (
+	j "JobOrch/internal/job"
+	"context"
+)
+
+type JobRepository interface {
+	Create(ctx context.Context, job j.Job) error
+	UpdateStatus(ctx context.Context, id string, status j.Status) error
+	SetError(ctx context.Context, id, err string) error
+	GetPending(ctx context.Context, limit int) ([]j.Job, error) // Возвращает задачи со статусом pending при падении приложения
+}
