3b2b496e9f71070acdb85a18fe3d91909f67a6d1
Michael <mvasilyev04@gmail.com>
2026-02-25 18:39:52 +0300

    done main for worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"JobOrch/adapters/postgres"
 	"JobOrch/internal/platform/workerpool"
 	"context"
+	"log"
+	"os"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	pool := workerpool.NewPool(ctx, 10)
-	
+	repo, err := postgres.NewPool(ctx, os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		log.Printf("%+v\n", err)
+		cancel()
+	}
+	for {
+		jobs, err := repo.GetPending(ctx, 10)
+		if err != nil {
+			log.Printf("%+v\n", err)
+			break
+		}
+		if len(jobs) == 0 {
+			break
+		}
+		for i := range jobs {
+			pool.Submit(ctx, &jobs[i])
+		}
+	}
 	cancel()
 	pool.ShutDown()
 }
