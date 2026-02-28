6dd00617469911732dd0707a762acb6e29d8c91d
Michael <mvasilyev04@gmail.com>
2026-03-01 02:18:41 +0300

    fix function declaration

--- internal/storage/interface.go
+++ internal/storage/interface.go
@@ -3,11 +3,13 @@ package storage
 import (
 	j "JobOrch/internal/job"
 	"context"
+
+	"github.com/google/uuid"
 )
 
 type JobRepository interface {
-	Create(ctx context.Context, record j.JobRecord) error
-	UpdateStatus(ctx context.Context, id string, status j.Status) error
-	SetError(ctx context.Context, id, pg_err string) error
+	Create(ctx context.Context, record *j.JobRecord) error
+	UpdateStatus(ctx context.Context, id uuid.UUID, status j.Status) error
+	SetError(ctx context.Context, id uuid.UUID, pg_err string) error
 	GetPending(ctx context.Context, limit int) ([]j.JobRecord, error) // Возвращает задачи со статусом pending при падении приложения
 }
