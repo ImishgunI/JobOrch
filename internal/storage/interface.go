a188e337ac28b03888a9817e79f651df9748f5e7
Michael <mvasilyev04@gmail.com>
2026-03-03 00:31:40 +0300

    just change package name

--- internal/storage/interface.go
+++ internal/storage/interface.go
@@ -1,15 +1,15 @@
 package storage
 
 import (
-	j "JobOrch/internal/job"
+	"JobOrch/internal/job"
 	"context"
 
 	"github.com/google/uuid"
 )
 
 type JobRepository interface {
-	Create(ctx context.Context, record *j.JobRecord) error
-	UpdateStatus(ctx context.Context, id uuid.UUID, status j.Status) error
+	Create(ctx context.Context, record *job.JobRecord) error
+	UpdateStatus(ctx context.Context, id uuid.UUID, status job.Status) error
 	SetError(ctx context.Context, id uuid.UUID, pg_err string) error
-	GetPending(ctx context.Context, limit int) ([]j.JobRecord, error) // Возвращает задачи со статусом pending при падении приложения
+	GetPending(ctx context.Context, limit int) ([]job.JobRecord, error) // Возвращает задачи со статусом pending при падении приложения
 }
